ecb72fc8f4599c5ddc8aba90f6ffacf9e623c588
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-10-17 08:48:47 +0700

    fix db

--- TelerikMvcAppDemo/Controllers/EmployeeController.cs
+++ TelerikMvcAppDemo/Controllers/EmployeeController.cs
@@ -1,13 +1,8 @@
 ﻿using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using TelerikMvcAppDemo.App_Start;
 using TelerikMvcAppDemo.Models;
-using TelerikMvcAppDemo.Models.SubModels;
 using TelerikMvcAppDemo.Repositories;
 
 namespace TelerikMvcAppDemo.Controllers
@@ -20,49 +15,41 @@ namespace TelerikMvcAppDemo.Controllers
         {
             _unitOfWork = unitOfWork;
         }
-        // GET: Employee
         public ActionResult Index()
         {
             return View("GetData");
         }
-
-        public static EmployeeEntities db = new EmployeeEntities();
-
         public JsonResult GetData([DataSourceRequest] DataSourceRequest request)
         {
             var emp = _unitOfWork.Employees.GetAll();
             return Json(emp.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
         }
-        public JsonResult Create_Employee([DataSourceRequest] DataSourceRequest request, Employee employee)
+        public JsonResult Create([DataSourceRequest] DataSourceRequest request, Employee employee)
         {
             if (employee != null && ModelState.IsValid)
             {
-                Employee emp = new Employee();
-                emp.EmployeeID = employee.EmployeeID;
-                emp.FirstName = employee.FirstName;
-                emp.LastName = employee.LastName;
-
-                _unitOfWork.Employees.Add(emp);
+                _unitOfWork.Employees.Add(employee);
                 _unitOfWork.Complete();
             }
             return Json(new[] { employee }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
         }
         [AcceptVerbs(HttpVerbs.Post)]
-        public JsonResult Update_Employee([DataSourceRequest] DataSourceRequest request, Employee employee)
+        public JsonResult Update([DataSourceRequest] DataSourceRequest request, Employee employee)
         {
             if (employee != null && ModelState.IsValid)
             {
                 Employee emp = _unitOfWork.Employees.GetById(employee.EmployeeID);
-                emp.EmployeeID = employee.EmployeeID;
-                emp.FirstName = employee.FirstName;
-                emp.LastName = employee.LastName;
+                emp.Name = employee.Name;
+                emp.Email = employee.Email;
+                emp.Address = employee.Address;
+                emp.Phone = employee.Phone;
 
                 _unitOfWork.Complete();
             }
             return Json(ModelState.IsValid ? true : ModelState.ToDataSourceResult());
         }
         [AcceptVerbs(HttpVerbs.Post)]
-        public JsonResult Delete_Employee([DataSourceRequest] DataSourceRequest request, Employee employee)
+        public JsonResult Delete([DataSourceRequest] DataSourceRequest request, Employee employee)
         {
             if (employee != null && ModelState.IsValid)
             {
--- TelerikMvcAppDemo/Models/Employee.cs
+++ TelerikMvcAppDemo/Models/Employee.cs
@@ -15,9 +15,10 @@ namespace TelerikMvcAppDemo.Models
     public partial class Employee
     {
         public int EmployeeID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Phone { get; set; }
     }
 }
